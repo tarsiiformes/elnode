a242ecf77fe344781f50d8a5d00991d4b91b6ac7
Jonas Bernoulli <jonas@bernoul.li>
2024-12-30 00:28:16 +0100

    Move metadata from elnode-pkg.el to elnode.el

--- elnode.el
+++ elnode.el
@@ -7,6 +7,8 @@
 ;; Maintainer: GitHub user "Jcaw"
 ;; Created: 5th October 2010
 ;; Keywords: lisp, http, hypermedia
+;; Package-Version: 0.9.9.8
+;; Package-Requires: ((web "0.1.4") (dash "1.1.0") (noflet "0.0.7") (s "1.5.0") (creole "0.8.14") (fakir "0.1.6") (db "0.0.5") (kv "0.0.17"))
 
 ;; This file is NOT part of GNU Emacs.
 
